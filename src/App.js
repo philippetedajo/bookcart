bd44f683aa6e5df9bea757f775e005ba7c9541f3
philippe <philmuse21@gmail.com>
2020-07-25 15:49:27 +0100

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
   Details,
   FidelityCoupon,
 } from "./components";
-
+import { Helmet } from "react-helmet";
 import styled from "styled-components";
 
 export class App extends Component {
@@ -71,35 +71,45 @@ export class App extends Component {
 
   render() {
     return (
-      <Content className="container">
-        <h1 style={{ fontSize: "55px", color: "teal" }} className="mb-4">
-          BookCart
-        </h1>
+      <>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>BookCart</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
+        <Content className="container">
+          <h1 style={{ fontSize: "55px", color: "teal" }} className="mb-4">
+            BookCart
+          </h1>
 
-        <SubTotal subPrice={this.state.subTotal.toFixed(2)} />
-        <Promotion reduction={this.state.reduction.toFixed(2)} />
-        <Taxe fees={this.state.fees.toFixed(2)} />
-        <Total price={this.state.total.toFixed(2)} />
-        <Details
-          price={this.state.total.toFixed(2)}
-          toggleDetail={this.state.isDetailShow}
-          //func props
-          onClikDetails={this.handleOnClickDetails}
-        />
-        <FidelityCoupon
-          toogleFc={this.state.isFcShow}
-          disableButton={this.state.isApplyDisable}
-          alert={this.state.alertMessage}
-          //func props
-          onClickFc={this.handleOnclickFc}
-          onClickApply={this.handleOnApply}
-          onChangeCode={this.handleOnChangeCode}
-        />
+          <SubTotal subPrice={this.state.subTotal.toFixed(2)} />
+          <Promotion reduction={this.state.reduction.toFixed(2)} />
+          <Taxe fees={this.state.fees.toFixed(2)} />
+          <Total price={this.state.total.toFixed(2)} />
+          <Details
+            price={this.state.total.toFixed(2)}
+            toggleDetail={this.state.isDetailShow}
+            //func props
+            onClikDetails={this.handleOnClickDetails}
+          />
+          <FidelityCoupon
+            toogleFc={this.state.isFcShow}
+            disableButton={this.state.isApplyDisable}
+            alert={this.state.alertMessage}
+            //func props
+            onClickFc={this.handleOnclickFc}
+            onClickApply={this.handleOnApply}
+            onChangeCode={this.handleOnChangeCode}
+          />
 
-        <p className="mb-2 mt-2" style={{ color: "#6c757d", fontSize: "15px" }}>
-          Fidelity Code : {this.state.originalCode}{" "}
-        </p>
-      </Content>
+          <p
+            className="mb-2 mt-2"
+            style={{ color: "#6c757d", fontSize: "15px" }}
+          >
+            Fidelity Code : {this.state.originalCode}{" "}
+          </p>
+        </Content>
+      </>
     );
   }
 }
